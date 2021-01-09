0e522afabba3f3c47ab3eed40ea89917491ccb3e
Prasansha Satpathy <56639527+Sara-cos@users.noreply.github.com>
2021-01-09 14:05:30 +0530

    Update train.py
    added the required lines

--- nd00333_AZMLND_Optimizing_a_Pipeline_in_Azure-Starter_Files/train.py
+++ nd00333_AZMLND_Optimizing_a_Pipeline_in_Azure-Starter_Files/train.py
@@ -13,14 +13,14 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
+web_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = Dataset.Tabular.from_delimited_files(path=web_path)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+x_train, x_test , y_train, y_test =  train_test_split(x,y)
 
 run = Run.get_context()
 
@@ -53,7 +53,7 @@ def clean_data(data):
 
 def main():
     # Add arguments to script
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description= 'training begin')
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
