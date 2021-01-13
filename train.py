84546db4d8ff6e089238c0c097305aa892a40f4c
Prasansha Satpathy <56639527+Sara-cos@users.noreply.github.com>
2021-01-14 00:26:10 +0530

    Update train.py

--- train.py
+++ train.py
@@ -73,8 +73,9 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
-    os.makedirs(./outputs, exist_ok = True)
+    os.makedirs('outputs', exist_ok = True)
     joblib.dump(value=model, filename ="./outputs/model.joblib")
+    
 
 if __name__ == '__main__':
     main()
